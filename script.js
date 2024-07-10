63fad579bfb60b7b15b02dec5e7d9ff411210f42
luna <101097846+lunaticsm@users.noreply.github.com>
2024-07-10 22:17:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    var message = document.getElementById('message');
+    if (message.style.display === 'none' || message.style.display === '') {
+        message.style.display = 'block';
+    } else {
+        message.style.display = 'none';
+    }
+}
+￼Enter
